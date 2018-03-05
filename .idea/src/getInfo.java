c1ce3bcc2ef1160a584d68071a10cc556d28a30e
kelbert2 <kristiannaelbert@gmail.com>
2018-03-05 14:29:57 -0500

    abstracting more

---
+++ .idea/src/getInfo.java
@@ -0,0 +1,8 @@
+public interface getInfo {
+    public String space();
+    public String searchTime();
+    public String unionTime();
+    public String insertTime();
+    public String deleteTime();
+    public String accessTime();
+}
